3f8199fb1a56023444a23f9e1dfd9346ebaf065e
Hydra <41239639+HalfHydra@users.noreply.github.com>
2022-03-17 00:34:27 -0400

    Push build

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace WindowsBackgroundDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Windows Background Extractor\n>");
+
+            string currentPath = Directory.GetCurrentDirectory();
+
+            string outputPath = Path.Combine(currentPath, "Output");
+
+            if (!Directory.Exists(Path.Combine(outputPath, "Desktop"))) Directory.CreateDirectory(Path.Combine(outputPath, "Desktop"));
+
+            if (!Directory.Exists(Path.Combine(outputPath, "Mobile"))) Directory.CreateDirectory(Path.Combine(outputPath, "Mobile"));
+
+            string CDNPath = "";
+
+            string[] subdirectories = Directory.GetDirectories(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Packages"));
+            foreach (string directory in subdirectories)
+            {
+                if(directory.IndexOf("Microsoft.Windows.ContentDeliveryManager") != -1) 
+                {
+                    CDNPath = directory;
+                    break;
+                }
+            }
+
+            string imagePath = Path.Combine(Path.Combine(CDNPath, "LocalState"), "Assets");
+
+            string targetPath = outputPath;
+            
+            if(Directory.Exists(imagePath)){
+                string[] files = Directory.GetFiles(imagePath);
+                foreach (string s in files)
+                {   
+                    string filePath = Path.Combine(imagePath, s);
+                    FileInfo info = new FileInfo(filePath);
+                    if(info.Length > 200000) //filter out the tile icons and bloatware
+                    {
+                        Bitmap bmp = new Bitmap(filePath);
+                        if (bmp.Width == 1920 && bmp.Height == 1080) {
+                            targetPath = Path.Combine(outputPath, "Desktop");
+                        } else {
+                            targetPath = Path.Combine(outputPath, "Mobile");
+                        }
+                        string fileName = Path.GetFileName(s) + ".png";
+                        string destFile = Path.Combine(targetPath, fileName);
+                        File.Copy(s, destFile, true);
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Windows10BackgroundDump
+# Windows 10 Spotlight Background Dumper
+
+If you have your lock screen on Windows 10 selected to use "Windows Spotlight", you see a brand new desktop background every morning. Since the file location is not in the easiest place to access, and you must rename the files to .png in order to see them, I created this to do it for me. It separates the mobile and desktop versions as well.
+
+I used to do this manually, so I figured this was a minor task I could make something for.
+
